4ad279c755d60beecb0603f8bb6374ff7423db96
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-11 11:55:40 +0530

    Create Area_of_Circle.c

---
+++ Semester 01/Area_of_Circle.c
@@ -0,0 +1,33 @@
+/*
+* A C program to calculate the area of the circle
+*/
+#include <stdio.h>
+#include <conio.h>
+#define pi 3.1415
+
+
+// Prototype declaration
+float Area(float radius);
+
+
+int main() {
+    // Declaring variables
+    float area, radius;
+
+    // Taking input of radius
+    printf("Enter the radius of circle: ");
+    scanf("%f", &radius);
+
+    // Calculating area and displaying output
+    area = Area(radius);
+    printf("\n• Area of circle : %.2f", area);
+
+    return 0;
+}
+
+// This function calculates area
+float Area(float radius){
+   float area;
+   area = 3.14 * radius_circle * radius_circle;
+   return area;
+}
